e5e1cff9127deb95676d39a461dd4deac2852879
Brian Chan <brian.chan1@network.lilly.com>
2024-12-06 16:51:07 -0500

    Copy day 1 as template

--- day6/part2.py
+++ day6/part2.py
@@ -1,17 +1,68 @@
+from enum import IntEnum
+import time
+import os
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+class Direction(IntEnum):
+    NORTH = 0
+    EAST = 1
+    SOUTH = 2
+    WEST = 3
+
+guardDirection = 0
+
+def find_guard(mapped_area):
+    for i in range(len(mapped_area)):
+        for j in range(len(mapped_area[0])):
+            if(mapped_area[i][j] == '^'):
+                return (i, j)
+    return (-1, -1)
+def print_map(mapped_area):
+    for line in mapped_area:
+        for char in line:
+            print(char, end='')
+        print("")
+def walk(mapped_area, guard_pos, guardDirection) :
+    # mark current position
+    mapped_area[guard_pos[0]][guard_pos[1]] = "X"
+    new_pos = calculate_delta(guardDirection, guard_pos)
+    if (new_pos[0] == -1 or new_pos[1] == -1):
+        return new_pos
+    if (new_pos[0] == len(mapped_area) or new_pos[1] == len(mapped_area[0])):
+        return (-1, -1)
+    if (mapped_area[new_pos[0]][new_pos[1]] == "#"):
+        return guard_pos
+    return new_pos
+
+def calculate_delta(direction, current_position) :
+    if(direction == Direction.NORTH) :
+        return (current_position[0] - 1, current_position[1])
+    
+    if(direction == Direction.EAST) :
+        return (current_position[0], current_position[1] + 1)
+    
+    if(direction == Direction.SOUTH) :
+        return (current_position[0] + 1, current_position[1])
+    
+    if(direction == Direction.WEST) :
+        return (current_position[0], current_position[1] - 1)
+    
 with open('input.txt', 'r') as file: 
-    first = []
-    second = []
+    mapped_area = []
     for line in file:
-        ids = line.split()
-        first.append(int(ids[0]))
-        second.append(int(ids[1]))
-    sum = 0
-    for i in range(0, len(first)):
-        search_value = first[i]
-        found_number = 0
-        for j in range(0, len(first)):
-            if (search_value == second[j]):
-                found_number += 1
-        # print("found " + str(found_number) + " of " + str(search_value))
-        sum += found_number * search_value
-    print(sum)
+        mapped_area.append(list(line.rstrip()))
+    guard_pos = find_guard(mapped_area)
+    # print_map(mapped_area)
+    guardDirection = Direction.NORTH
+    while (guard_pos[0] != -1 and guard_pos[1] != -1) :
+        old_pos = (guard_pos[0], guard_pos[1])
+        guard_pos = walk(mapped_area, guard_pos, guardDirection)
+        if(old_pos == guard_pos) :
+            guardDirection =  (guardDirection + 1) % 4
+            print(guardDirection)
+    x_count = 0
+    for line in mapped_area:
+        x_count += line.count("X")
+    print(x_count)
