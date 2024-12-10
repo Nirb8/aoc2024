e5622b063a20ba5cd3101a9a677d4a71039a6311
rhit-chanb <chan992599@gmail.com>
2024-12-09 22:52:22 -0500

    Day 8 part 1 done, use as template for part 2

--- day8/part1.py
+++ day8/part1.py
@@ -8,13 +8,14 @@ def get_unique_pairs(pairs_list):
     for pair in pairs_list :
         for pair_two in pairs_list :
             current_pair = (pair, pair_two)
-            if (current_pair not in pair_pairs):
+            if (current_pair not in pair_pairs and (current_pair[0], current_pair[1]) not in pair_pairs):
                 pair_pairs.append(current_pair)
+    return pair_pairs
 
-
-with open('test.txt', 'r') as file: 
+with open('input.txt', 'r') as file: 
     city_map = []
     signal_beacons = {}
+    antinodes = {}
     for line in file:
         city_map.append(list(line.rstrip()))
     for i in range(0, len(city_map)):
@@ -27,12 +28,44 @@ with open('test.txt', 'r') as file:
                     locations = []
                     locations.append((i, j))
                     signal_beacons[tile] = locations
-    for key in signal_beacons.keys:
+    for key in signal_beacons.keys():
         values = signal_beacons[key]
         pair_pairs = get_unique_pairs(values)
         for pair_pair in pair_pairs:
             first_pos = pair_pair[0]
             second_pos = pair_pair[1]
-
+            if (first_pos == second_pos) :
+                continue
+            first_dist = first_pos[0] - second_pos[0]
+            second_dist = first_pos[1] - second_pos[1]
+            # print('first_pos')
+            # print(first_pos)
+            # print('second_pos')
+            # print(second_pos)
+            # print('first_dist')
+            # print(first_dist)
+            # print('second_dist')
+            # print(second_dist)
+            
+            firstAntiNode = (first_pos[0] + first_dist, first_pos[1] + second_dist)
+            secondAntiNode = (second_pos[0] - first_dist, second_pos[1] - second_dist)
+            if (firstAntiNode in antinodes) :
+                antinodes[firstAntiNode] += 1
+            else :
+                antinodes[firstAntiNode] = 1
+            if (secondAntiNode in antinodes) :
+                antinodes[secondAntiNode] += 1
+            else :
+                antinodes[secondAntiNode] = 1
+    toDel = []
+    for antinode in antinodes:
+        if (antinode[0] < 0 or antinode[1] < 0 or antinode[0] >= len(city_map) or antinode[1] >= len(city_map[0])):
+            toDel.append(antinode)
+    for antinode in toDel:
+        del antinodes[antinode]
+    for antinode in antinodes:
+        print(antinode)
+        city_map[antinode[0]][antinode[1]] = "#"
     print(signal_beacons)
     print_map(city_map)
+    print(len(antinodes))
